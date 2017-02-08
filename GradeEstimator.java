04329043f5674920a416eda90ac6c306611f878f
mquinlan2 <mquinlan2@users.noreply.github.com>
2017-02-08 16:44:21 -0600

    Add files via upload

---
+++ GradeEstimator.java
@@ -0,0 +1,13 @@
+
+public class GradeEstimator {
+
+	public GradeEstimator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
